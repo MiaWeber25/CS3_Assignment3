e53df8a0ad68a06f07810e5854cca427ca67794b
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2022-10-25 16:29:11 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment 3
+//
+//  Created by Brandon Kamplain on 10/25/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
