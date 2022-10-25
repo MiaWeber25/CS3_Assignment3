3b5807a2456bec34c2010370e5ecbe72f12472e9
bkamplai <diadems.plunge-0p@icloud.com>
2022-10-25 16:58:49 -0600

    main

--- main.cpp
+++ main.cpp
@@ -6,9 +6,13 @@
 //
 
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ifstream in_file("A Scandal in Bohemia.txt", ios::binary);
+    in_file.seekg(0, ios::end);
+    int file_size = in_file.tellg();
+    cout<<"Size of the file is"<<" "<< file_size<<" "<<"bytes";
     return 0;
 }
