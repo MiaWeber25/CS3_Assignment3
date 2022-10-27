4139311b688977a4d758a067bd14d06e79147ffd
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2022-10-26 20:07:55 -0600

    Got it to work finally

--- Horspool.cpp
+++ Horspool.cpp
@@ -6,61 +6,60 @@
 //
 
 #include <iostream>
-#include <chrono>
 using namespace std;
-using namespace chrono;
 
 #define numOfChars 256
 
-int ShiftTable(char []);
-int HorspoolMatching(char [], char []);
+void ShiftTable(char*, int, int[]);
+void HorspoolMatching(char*, char*);
 
+/*
 int main()
 {
-    char txt[]= "ABAAABCD";
+    char txt[] = "ABAAABCD";
 	char pat[] = "ABC";
-	cout << HorspoolMatching(pat, txt) << endl;
+	HorspoolMatching(pat, txt);
 
     return 0;
 }
+*/
 
-int ShiftTable(char P[])
+void ShiftTable(char *P, int size, int table[numOfChars])
 {
     int index = 0;
-    int m = sizeof(P)/sizeof(P[0]);
 
-    int table[m];
-
-    while (index < m-1)
-    {
-        table[P[index]] = m - 1 - index;
-        index++;
-    }
-    return *table;
+    for(index = 0; index < numOfChars; index++)
+        table[index] = -1;
+    
+    for(index = 0; index < size; index++)
+        table[(int) P[index]] = index;
+    
 }
 
-int HorspoolMatching(char P[], char T[])
+void HorspoolMatching(char *P, char *T)
 {
-    int m = sizeof(P)/sizeof(P[0]);
-    int n = sizeof(T)/sizeof(T[0]);
-    int table[m];
-    
-    for (int i = 0; i < m; i++)
-        table[i] = ShiftTable(&P[i]);
-    
-    int j = m-1;
+    int m = strlen(P);
+    int n = strlen(T);
+    int table[numOfChars];
 
-    while (j <= n-1)
+    ShiftTable(P, m, table);
+
+    int shift = 0;
+    
+    while(shift <= (n-m))
     {
-        int k = 0;
-        while ((k <= m-1) && P[m-1-k] == T[j-k])
+        int j = m-1;
+
+        while(j >= 0 && P[j] == T[shift+j])
+            j--;
+        
+        if(j < 0)
         {
-            k++;
+            cout << "pattern occurs at shift " << shift << endl;
+
+            shift += (shift+m < n)? m-table[T[shift+m]] : 1;
         }
-        if (k == m)
-			return (j - m + 1);
-		else
-			j = j + table[T[j]];
+        else
+            shift += max(1, j - table[T[shift+j]]);
     }
-    return -1;
 }
