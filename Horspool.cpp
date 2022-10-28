de2bec7c60f273c58ee2033ee8cfab21e6d80738
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2022-10-27 19:51:59 -0600

    Cleaned up a bit, added comments

--- Horspool.cpp
+++ Horspool.cpp
@@ -22,18 +22,22 @@ void HorspoolMatching(char*, char*);
     return 0;
 }*/
 
+// The preprocessing function for Horspool's Shift Table
 void ShiftTable(char *P, double size, int table[numOfChars])
 {
     int index = 0;
 
+    // Initialize all occurrences as -1
     for(index = 0; index < numOfChars; index++)
         table[index] = -1;
     
+    // Fill the actual value of last occurrence of a character
     for(index = 0; index < size; index++)
         table[(int) P[index]] = index;
     
 }
 
+// Pattern searching function using ShiftTable()
 void HorspoolMatching(char *P, char *T)
 {
     int m = strlen(P);
@@ -43,26 +47,32 @@ void HorspoolMatching(char *P, char *T)
     int cnt = 0;
     int cmp = 0;
 
+    // Fill the bad character array by calling the preprocessing function ShiftTable() for a given pattern
     ShiftTable(P, m, table);
 
+    //shift of the pattern with respect to text
     int shift = 0;
     
     while(shift <= (n-m))
     {
         cmp++;
+
         int j = m-1;
 
+        // Keep reducing index j of pattern while characters of pattern and text are matching at this shift
         while(j >= 0 && P[j] == T[shift+j])
             j--;
         
+        // f the pattern is present at current shift, then index j will become -1 after the above loop
         if(j < 0)
         {
-            //cout << "pattern occurs at shift " << shift << endl;
             cnt++;
 
+            // Shift the pattern so that the next character in text aligns with the last occurrence of it in pattern. The condition shift + m < n is necessary for the case when pattern occurs at the end of text
             shift += (shift+m < n)? m-table[T[shift+m]] : 1;
         }
         else
+            // Shift the pattern so that the bad character in text aligns with the last occurrence of it in pattern. The max function is used to make sure that we get a positive shift. We may get a negative shift if the last occurrence of bad character in pattern is on the right side of the current character.
             shift += max(1, j - table[T[shift+j]]);
     }
 
